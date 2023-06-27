2b550866155cf938f829d11e3e73c6bbabb35628
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-06-27 16:18:34 -0400

    more photo dump

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,52 +3,162 @@ export const galleryContent = {
         one : {
             title: 'Dire Wolf',
             description: 'Dire Wolf came about one day as I was swiping other animals on canvas. I had decided that I wanted to do a swiped animal series and I did a couple of foxes to start. Dire Wolf popped up in my head but because of my schedule I was not able to paint for at least a couple of weeks. He was designed virtually over and over again. By the time I was actually able to paint he just flowed right out of the brush and swiping technique. I think my love of animals helps me create the different characteristics my swiped animals portray.',
-            photo: 'direwolf'
+            photo: 'direwolf.png'
         },
         two : {
-            title: 'a',
-            description: 'a',
-            photo: 'a'
+            title: 'Cow Commission (NFS)',
+            description: 'Other Swiped animals that can be on a button to click into with Dire Wolf as the face of them.',
+            photo: 'cow.png'
         },
         three : {
-            title: 'a',
-            description: 'a',
-            photo: 'a'
+            title: 'Starfish Swipe',
+            description: '',
+            photo: 'starfish.png'
         },
         four : {
-            title: 'a',
-            description: 'a',
-            photo: 'a'
+            title: 'Foxy Commission (NFS)',
+            description: '',
+            photo: 'halffox.png'
         },
         five : {
-            title: 'a',
-            description: 'a',
-            photo: 'a'
+            title: 'Its A Jungle Out There Commission (NFS)',
+            description: '',
+            photo: 'elephant.png'
         },
         six : {
-            title: 'a',
-            description: 'a',
-            photo: 'a'
+            title: 'Horse Commission (NFS)',
+            description: '',
+            photo: 'horse.png'
         },
         seven : {
-            title: 'a',
-            description: 'a',
-            photo: 'a'
+            title: 'Foxy (NFS)',
+            description: 'CloudFolios Marketplace Gallery Acceptance for `Foxy` July 2022',
+            photo: 'fox.png'
         },
         eight : {
-            title: 'a',
-            description: 'a',
-            photo: 'a'
+            title: 'Planitarium 2022',
+            description: '',
+            photo: 'leaf-blackbg.png'
         },
         nine : {
-            title: 'a',
-            description: 'a',
-            photo: 'a'
+            title: '',
+            description: '',
+            photo: 'leaf-greenbg.png'
         },
         ten : {
-            title: 'a',
-            description: 'a',
-            photo: 'a'
-        }
+            title: 'The Old Tree',
+            description: 'Accepted `The Old Tree` Into Our Online Public Marketplace Gallery Aug 2022. Exhibizone Grand Prize (EGP) 2022 international online group exhibition.',
+            photo: 'tree-blue-one.png'
+        },
+        eleven : {
+            title: '',
+            description: '',
+            photo: 'tree-amber-one.png'
+        },
+        twelve : {
+            title: '',
+            description: '',
+            photo: 'tree-blue-two.png'
+        },
+        thirteen : {
+            title: '',
+            description: '',
+            photo: 'tree-blue-three.png'
+        },
+        fourteen : {
+            title: '',
+            description: '',
+            photo: 'forest-grey.png'
+        },
+        fifteen : {
+            title: '',
+            description: '(NFS)',
+            photo: 'tree-psychadelic.png'
+        },
+        sixteen : {
+            title: '',
+            description: '',
+            photo: 'tree-blue-four.png'
+        },
+        seventeen : {
+            title: '',
+            description: '(NFS)',
+            photo: 'forest-fire.png'
+        },
+        eighteen : {
+            title: '',
+            description: '(NFS)',
+            photo: 'beach.png'
+        },
+        nineteen : {
+            title: '',
+            description: '',
+            photo: 'table-blue.png'
+        },
+        twenty : {
+            title: '',
+            description: '',
+            photo: 'table-half.png'
+        },
+        twentyone : {
+            title: '',
+            description: 'Original (NFS)',
+            photo: 'pirateship.png'
+        },
+        twentytwo : {
+            title: '',
+            description: '',
+            photo: 'pirateship-two.png'
+        },
+        twentythree : {
+            title: '',
+            description: '(NFS)',
+            photo: 'greenman-case.png'
+        },
+        twentyfour : {
+            title: '',
+            description: 'Evergreen swiped on a record',
+            photo: 'evergreen-record.png'
+        },
+        twentyfive : {
+            title: '',
+            description: '',
+            photo: 'tree-white.png'
+        },
+        twentysix : {
+            title: '',
+            description: '',
+            photo: ''
+        },
+        twentyseven : {
+            title: '',
+            description: '',
+            photo: ''
+        },
+        twentyeight : {
+            title: '',
+            description: '',
+            photo: ''
+        },
+        twentynine : {
+            title: '',
+            description: '',
+            photo: ''
+        },
+        thirty : {
+            title: '',
+            description: '',
+            photo: ''
+        },
+        thirtyone : {
+            title: '',
+            description: '',
+            photo: ''
+        },
+        thirtytwo : {
+            title: '',
+            description: '',
+            photo: ''
+        },
     }
 }
