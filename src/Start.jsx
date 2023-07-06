b12e713e81bfcde7c643453c86a53a9a6617f2b4
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-07-06 11:41:57 -0400

    instagram logo w/ address added

--- src/Start.jsx
+++ src/Start.jsx
@@ -1,9 +1,16 @@
-
+import instagram from './assets/instagram.png'
 
 function Start() {
   return (
     <div className="start">
-        Fran Norsworthy
+        <div>
+            Fran Norsworthy
+        </div>
+        <div>
+            <a href='https://www.instagram.com/frannorsworthy/' className='instagram'>
+                <img src={instagram} alt='instagram' />
+            </a>
+        </div>
     </div>
   )
 }
