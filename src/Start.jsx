3b5c4395c4e4664e1e69f05a931375c4e7184a97
Duke Norsworthy <duke.norsworthy@gmail.com>
2023-07-07 10:58:54 -0400

    added email button

--- src/Start.jsx
+++ src/Start.jsx
@@ -1,4 +1,5 @@
 import instagram from './assets/instagram.png'
+import email from './assets/email.png'
 
 function Start() {
   return (
@@ -9,6 +10,9 @@ function Start() {
         <div>
             <a href='https://www.instagram.com/frannorsworthy/' className='instagram'>
                 <img src={instagram} alt='instagram' />
+            </a>
+            <a href="mailto: wench920@gmail.com" className='instagram'>
+                <img src={email} alt='email' />
             </a>
         </div>
     </div>
